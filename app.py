6d04af33f01712547bf0c59fc10b1d8398d65c91
Marc Brown <marcsmobile.brown@googlemail.com>
2023-11-12 15:18:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,56 @@
 import re
 import os
+import sys
+import difflib
+
+def adjust_comment_indentation(lines):
+    new_lines = []  # Ensure new_lines is initialized
+    for i, line in enumerate(lines):
+        stripped_line = line.lstrip()
+        if stripped_line.startswith("#"):
+            next_line_index = i + 1
+            while next_line_index < len(lines) and not lines[next_line_index].strip():
+                next_line_index += 1
+            if next_line_index < len(lines):
+                next_line_indentation = len(lines[next_line_index]) - len(lines[next_line_index].lstrip())
+                line = " " * next_line_indentation + stripped_line
+        new_lines.append(line)
+    return new_lines  # Return the new_lines list
+
 
 def process_yaml_files(directory, file_extension=".eyaml"):
     for filename in os.listdir(directory):
         if filename.endswith(file_extension):
             filepath = os.path.join(directory, filename)
-            
-            # Read the contents of the file
             with open(filepath, 'r', encoding='utf-8') as file:
-                lines = file.readlines()
+                original_lines = file.readlines()
 
-            # Normalize line endings and ensure document start marker
-            lines = [line.replace('\r\n', '\n') for line in lines]  # Convert line endings to Unix style
-            if lines and not lines[0].strip().startswith('---'):
-                lines.insert(0, '---\n')
+            modified_lines = [line.rstrip() + '\n' for line in original_lines]
+            if modified_lines and not modified_lines[0].strip().startswith('---'):
+                modified_lines.insert(0, '---\n')
 
-            # Ensure space after '#' in comments
-            lines = [re.sub(r'(^#)([^\s])', r'# \2', line) for line in lines]
+            modified_lines = adjust_comment_indentation(modified_lines)
+            if modified_lines[-1].strip() == '---':
+                modified_lines.pop()
 
-            # Remove excess blank lines at the beginning
-            while lines and lines[0].strip() == '':
-                lines.pop(0)
+            modified_lines = [line for i, line in enumerate(modified_lines) if i == 0 or not (line.isspace() and modified_lines[i - 1].isspace())]
 
-            # Write the changes back to the file
             with open(filepath, 'w', encoding='utf-8') as file:
-                file.writelines(lines)
+                file.writelines(modified_lines)
 
-            print(f"Processed {filename}")
+            # Create and write the change report
+            change_report = difflib.unified_diff(original_lines, modified_lines, fromfile=filename, tofile=filename, lineterm='\n')
+            change_filename = os.path.splitext(filename)[0] + '-changes.txt'
+            change_filepath = os.path.join(directory, change_filename)
+            with open(change_filepath, 'w', encoding='utf-8') as change_file:
+                change_file.writelines(change_report)
 
-# Define the path to your directory containing YAML files
-yaml_directory = "/home/marc/Downloads/nodes/"  # Replace with your directory path
+            print(f"Processed {filename}. Change report written to {change_filename}")
 
-# Run the function on your directory
-process_yaml_files(yaml_directory)
+if __name__ == "__main__":
+    # Get the directory path from command line argument
+    if len(sys.argv) > 1:
+        yaml_directory = sys.argv[1]
+        process_yaml_files(yaml_directory)
+    else:
+        print("Please provide the directory path as an argument.")
